751c609c43720c52a7ff43aeca93c22639f6113d
Eyüp Fidan <info@eyupfidan.com>
2022-09-21 11:39:06 +0300

    Add Login & Register Page With Handlebars

--- app.js
+++ app.js
@@ -1,32 +1,44 @@
 const express = require('express')
 const app = express()
-const  { engine } = require('express-handlebars') ;
+const {
+    engine
+} = require('express-handlebars');
 const port = 3000
 const hostname = '127.0.0.1'
-const path = require('path')
 app.use(express.static('public'))
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
-app.get('/' , (req,res) => {
-res.status(200)
-res.render('site/index')
+app.get('/', (req, res) => {
+    res.status(200)
+    res.render('site/index')
 })
 
-app.get('/about' , (req,res) => {
+app.get('/about', (req, res) => {
     res.status(200)
     res.render('site/about')
-}) 
+})
 
 
-app.get('/blog' , (req,res) => {
-        res.status(200)
-        res.render('site/about')
-    })
-        
+app.get('/blog', (req, res) => {
+    res.status(200)
+    res.render('site/about')
+})
 
-app.listen(port,hostname, () => {
+app.get('/contact', (req, res) => {
+    res.status(200)
+    res.render('site/contact')
+})
+app.get('/login', (req, res) => {
+    res.status(200)
+    res.render('site/login')
+})
+app.get('/register', (req, res) => {
+    res.status(200)
+    res.render('site/register')
+})
+app.listen(port, hostname, () => {
     console.log(`Server is Running http://${hostname}:${port}/`)
 })
